f8baaabb33e436f35bf96ad2452947c4f05e96b0
Pier Paolo Tarasco <alu0101371573@ull.edu.es>
2019-11-20 20:25:47 +0000

    Add test for empty list, head and tail should be nil

--- spec/prct06/list_spec.rb
+++ spec/prct06/list_spec.rb
@@ -1,10 +1,26 @@
 include Prct06::Bundler
 
 RSpec.describe List do
+
 	context "with empty list" do
+		before :each do
+			@l = List.new
+		end
+
 		it "should exists the methods to get head and tail" do
 			expect(List.method_defined? :head).to eq true
 			expect(List.method_defined? :tail).to eq true
 		end
+
+		it "should be able to add a node" do
+
+		end
+
+		it "head and tail should be nil" do
+			expect(@l.head == nil).to eq true
+			expect(@l.tail == nil).to eq true
+		end
 	end
+
+
 end
